73189e54d33280dc67c260369361fc33f0dfa207
karthikamalakannan <karthigakamalakannan4@gmail.com>
2026-06-18 14:19:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@
 // Step by step instructions are in SETUP_GUIDE.md
 // ============================================
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://lzftnetfqyokvjbmcicg.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZnRuZXRmcXlva3ZqYm1jaWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3NTE5MTIsImV4cCI6MjA5NzMyNzkxMn0.NN-9BuSiKKeA8CAN3lXlDHXSn2r4l-QsD2d1iunpHzs";
 
 // This is the password YOU use to unlock your inbox + photo locker admin view.
 // Change it to something only you know.
-const ADMIN_PASSWORD = "karthi2024";
+const ADMIN_PASSWORD = "Athiveerankarthi";
 
 // ============================================
 // Do not edit below this line
